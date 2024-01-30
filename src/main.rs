6a374975f965051a341de79a40ba79e9205b518d
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2024-01-30 10:38:25 +0100

    better terminal output; better file handling; cmd docs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod config;
 use anyhow::Result;
 use clap::{command, Parser};
 use config::Config;
+use console::{style, Term};
 use headless_chrome::{
     protocol::cdp::{
         Page::{CaptureScreenshotFormatOption, Viewport},
@@ -11,9 +12,11 @@ use headless_chrome::{
 };
 use std::{
     fs,
+    io::{self, Write},
     path::{Path, PathBuf},
 };
 
+/// Simply screenshot websites from your terminal
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
@@ -21,20 +24,26 @@ struct Cli {
     #[arg(short, long)]
     config: Option<String>,
 
+    /// URL of the web page to be captured
     url: String,
 
-    #[arg(short, long, default_value = "screenshot.png")]
-    output: PathBuf,
+    /// Output directory or file name
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 
+    /// Screen width
     #[arg(short = 'W', long)]
     width: Option<u32>,
 
+    /// Screen height
     #[arg(short = 'H', long)]
     height: Option<u32>,
 
+    /// Scale factor
     #[arg(short, long)]
     scale: Option<f64>,
 
+    /// Wait for DOM element (query selector)
     #[arg(long, default_value = "body")]
     wait_for: String,
 }
@@ -44,17 +53,23 @@ fn main() -> Result<()> {
 
     let cfg = Config::parse(cli.config)?;
 
-    let width = cli.width.or(cfg.default_width).unwrap_or(1920);
-    let height = cli.width.or(cfg.default_height).unwrap_or(1089);
+    let term = Term::stdout();
+
+    let orig_width = cli.width.or(cfg.default_width).unwrap_or(1920);
+    let orig_height = cli.height.or(cfg.default_height).unwrap_or(1080);
     let scale = cli.scale.or(cfg.default_scale).unwrap_or(1.0);
 
-    let width = width as f64 / scale;
-    let height = height as f64 / scale;
+    let width = orig_width as f64 / scale;
+    let height = orig_height as f64 / scale;
+
+    write_line(&term, &style("Open browser ...").italic().to_string());
 
     let browser = Browser::default()?;
 
+    write_line(&term, &style("Open new tab ...").italic().to_string());
+
     let tab = browser.new_tab_with_options(CreateTarget {
-        url: cli.url,
+        url: cli.url.clone(),
         width: Some(width as u32),
         height: Some(height as u32),
         browser_context_id: None,
@@ -62,6 +77,16 @@ fn main() -> Result<()> {
         new_window: None,
         background: None,
     })?;
+
+    write_line(
+        &term,
+        &style(format!(
+            "Waiting for element '{}' ...",
+            style(&cli.wait_for).cyan()
+        ))
+        .italic()
+        .to_string(),
+    );
 
     tab.wait_for_element(&cli.wait_for)?;
 
@@ -73,14 +98,45 @@ fn main() -> Result<()> {
         scale,
     };
 
+    let output_path = match cli.output {
+        Some(p) if p.is_dir() => p.join(default_file_name(&cli.url, orig_width, orig_height)),
+        Some(p) => p,
+        _ => default_file_name(&cli.url, orig_width, orig_height),
+    };
+
+    write_line(
+        &term,
+        &style("Capturing screenshot ...").italic().to_string(),
+    );
+
     let image_data = tab.capture_screenshot(
-        image_type_from_file_name(&cli.output),
+        image_type_from_file_name(&output_path),
         None,
         Some(viewport),
         true,
     )?;
 
-    fs::write(&cli.output, image_data)?;
+    write_line(
+        &term,
+        &style(format!(
+            "Writing screenshot to '{}' ...",
+            style(&output_path.to_string_lossy()).cyan()
+        ))
+        .italic()
+        .to_string(),
+    );
+
+    fs::write(&output_path, image_data)?;
+
+    write_line(
+        &term,
+        &style(format!(
+            "Successfully saved screenshot to '{}'.\n",
+            style(&output_path.to_string_lossy()).cyan()
+        ))
+        .green()
+        .to_string(),
+    );
 
     Ok(())
 }
@@ -92,4 +148,27 @@ fn image_type_from_file_name<P: AsRef<Path>>(path: P) -> CaptureScreenshotFormat
         Some("webp") => CaptureScreenshotFormatOption::Webp,
         None | Some(_) => CaptureScreenshotFormatOption::Png,
     }
+}
+
+fn canonicalize_url(url: &str) -> &str {
+    let mut url = url
+        .strip_prefix("https://")
+        .or_else(|| url.strip_prefix("http://"))
+        .unwrap_or(url);
+
+    if let Some(i) = url.find('/') {
+        url = &url[..i];
+    }
+
+    url
+}
+
+fn default_file_name(url: &str, width: u32, height: u32) -> PathBuf {
+    format!("{}-{width}x{height}.png", canonicalize_url(url)).into()
+}
+
+fn write_line(term: &Term, v: &str) {
+    _ = term.clear_line();
+    print!("{v}");
+    _ = io::stdout().flush();
 }
